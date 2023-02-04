18c6ae359a71ef51e1d6100708ec2a41fa5ae243
Johnny Valles <vallesja@uci.edu>
2023-02-04 09:04:50 -0800

    add driver

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Missing program arguments.");
+            System.err.println("You must provide an init file & file containing virtual addresses.");
+            System.exit(-1);
+        }
+
+        String initFile = args[0];
+        String vaFile = args[1];
+
+        VirtualMemoryManager vmm = new VirtualMemoryManager();
+        vmm.init(initFile);
+        vmm.translateFromFile(vaFile);
+    }
+}
