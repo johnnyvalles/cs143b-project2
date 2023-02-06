5191659d1d118ecb07bd9ab58ca14222b4e798b8
Johnny Valles <vallesja@uci.edu>
2023-02-06 14:32:55 -0800

    update driver, accept vas from stdin

--- Main.java
+++ Main.java
@@ -1,16 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.err.println("Missing program arguments.");
-            System.err.println("You must provide an init file & file containing virtual addresses.");
+        VirtualMemoryManager vmm = new VirtualMemoryManager();
+        Scanner scanner;
+
+        if (args.length < 1 || args.length > 2) {
+            System.err.println("You must provide an init file & optional file containing virtual addresses.");
             System.exit(-1);
         }
 
-        String initFile = args[0];
-        String vaFile = args[1];
+        if (args.length == 2) {
+            vmm.init(args[0]);
+            vmm.translateFromFile(args[1]);
+        }
 
-        VirtualMemoryManager vmm = new VirtualMemoryManager();
-        vmm.init(initFile);
-        vmm.translateFromFile(vaFile);
+        if (args.length == 1) {
+            vmm.init(args[0]);
+            scanner = new Scanner(System.in);
+            int virtualAddress;
+            while (scanner.hasNextInt()) {
+                virtualAddress = scanner.nextInt();
+                System.out.println("**********************************************");
+                System.out.println("Virtual Address:  " + virtualAddress);
+                System.out.println("Physical Address: " + vmm.translateVAtoPA(virtualAddress));
+                System.out.println("**********************************************");
+            }
+        }
     }
 }
