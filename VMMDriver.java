c696563b5d6f5651456d95ce9edc517287412052
Johnathon Valles <vallesja@uci.edu>
2023-02-07 06:05:44 +0000

    renamed Main.java to VMMDriver.java

--- VMMDriver.java
+++ VMMDriver.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class VMMDriver {
     public static void main(String[] args) {
         VirtualMemoryManager vmm = new VirtualMemoryManager();
         Scanner scanner;
