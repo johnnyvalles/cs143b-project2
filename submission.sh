5a83bb57254b776bc82d5ec62cc684375e95463a
Johnny Valles <vallesja@uci.edu>
2023-03-16 14:24:59 -0700

    create submission script

---
+++ submission.sh
@@ -0,0 +1,11 @@
+/bin/rm -f *.class
+/bin/rm -rf ~/cs143b-project2-submission-vallesja
+/bin/rm -f ~/output-dp.txt
+/bin/mkdir ~/cs143b-project2-submission-vallesja
+/bin/cp *.java ~/cs143b-project2-submission-vallesja
+/bin/cp README.md ~/cs143b-project2-submission-vallesja
+
+javac *.java && java VMMDriver test/dp/init-dp.txt test/dp/input-dp.txt > output-dp.txt
+/bin/cp ./output-dp.txt ~/
+/bin/rm -f *.class
+/bin/rm -f output-dp.txt
