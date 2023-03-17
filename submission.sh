1f3902e6d585c3a5a01aa1bb643877ae8d6649af
Johnny Valles <vallesja@uci.edu>
2023-03-16 18:35:09 -0700

    remove zip if exists

--- submission.sh
+++ submission.sh
@@ -1,6 +1,7 @@
 /bin/rm -f *.class
 /bin/rm -rf ~/cs143b-project2-submission-vallesja
 /bin/rm -f ~/output-dp.txt
+/bin/rm -f ~/cs143b-project2-submission-vallesja.zip
 /bin/mkdir ~/cs143b-project2-submission-vallesja
 /bin/cp *.java ~/cs143b-project2-submission-vallesja
 /bin/cp README.md ~/cs143b-project2-submission-vallesja
